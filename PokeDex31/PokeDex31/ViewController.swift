8cb69a84c2017d388ddfc7c38c071b93fbc917a0
jon <jdcorn@live.com>
2020-01-21 10:17:14 -0700

    first commit

---
+++ PokeDex31/PokeDex31/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex31
+//
+//  Created by Jon Corn on 1/21/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
